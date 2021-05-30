10e25faed4f4c0a88144f001da40ab58d5e4b14c
Sergey Serenko <sergey@thirstysprout.com>
2021-05-30 23:04:43 +0200

    add basic benchmarking

--- speed_test.go
+++ speed_test.go
@@ -19,3 +19,16 @@ func Test_checkConnection_WrongProvider(t *testing.T) {
 	_, _, err := CheckConnection("Google")
 	assert.NotNil(t, err)
 }
+
+func BenchmarkConnectionOokla(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, _, err := CheckConnection("Ookla")
+		assert.Nil(b, err)
+	}
+}
+func BenchmarkConnectionFast(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, _, err := CheckConnection("Fast")
+		assert.Nil(b, err)
+	}
+}
